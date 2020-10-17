09f7f13fd0cf9b6be29b787812c2bc447af0c6d1
Arden Rasmussen <ardenisthebest@gmail.com>
2020-10-17 13:14:44 -0700

    Added spicetify

--- thema-generate.py
+++ thema-generate.py
@@ -14,15 +14,9 @@ def generate_data(base):
         output[f"Hex {key}"] = val[1:]
     return output
 
-
-def main():
-    parser = ArgumentParser('thema-generate')
-    parser.add_argument('source', help='Theme definition JSON file')
-
-    args = parser.parse_args()
-
+def generate_templates(source):
     color_data = {}
-    with open(args.source) as template:
+    with open(source) as template:
         color_data = json.load(template)
 
     data = generate_data(color_data)
@@ -33,10 +27,6 @@ def main():
         output = f"./{color_data['name']}/{color_data['variant']}/"
     else:
         output = f"./{color_data['name']}/"
-
-    # __import__('pprint').pprint(data)
-    # __import__('pprint').pprint(templates)
-    # __import__('pprint').pprint(output)
 
     for template in templates:
         outfile = f"{output}{os.path.basename(template)}"
@@ -52,6 +42,19 @@ def main():
                 os.makedirs(os.path.dirname(outfile))
             with open(outfile, 'w') as outfile:
                 outfile.write(ren)
+
+def main():
+    parser = ArgumentParser('thema-generate')
+    parser.add_argument('source', nargs='*', help='Theme definition JSON file')
+
+    args = parser.parse_args()
+
+    if len(args.source) == 0:
+        args.source = glob.glob('./**/_colors.json', recursive=True)
+
+    for source in args.source:
+        generate_templates(source)
+
 
 
 
